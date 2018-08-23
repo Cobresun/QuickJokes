6866f5573b95516d392070c1dca948b104c90415
Wesley <hexlash14wes@gmail.com>
2018-08-22 22:17:56 -0600

    Defined CardView Interface adn started its implementation.

--- app/src/main/java/com/cobresun/quickjokes/View/CardView.java
+++ app/src/main/java/com/cobresun/quickjokes/View/CardView.java
@@ -1,4 +1,29 @@
 package com.cobresun.quickjokes.View;
 
+/**
+ * Interface for the display of cards.
+ *
+ */
 public interface CardView {
+
+    /**
+     *  Presents a card to the user.
+     */
+    public void displayCard();
+
+    /**
+     *
+     */
+    public void displayGuide();
+
+    /**
+     *  Reacts to the card being tapped.
+     */
+    public void onTapCard();
+
+    /**
+     * Reacts to a card being dismissed.
+     */
+    public void dismissCard();
+
 }
---
+++ app/src/main/java/com/cobresun/quickjokes/View/CardViewImpl.java
@@ -0,0 +1,32 @@
+package com.cobresun.quickjokes.View;
+
+public class CardViewImpl implements CardView {
+
+    /**
+     *  Takes a card and then displays the 
+     */
+    public void displayCard(){
+
+    }
+
+    /**
+     *
+     */
+    public void displayGuide(){
+
+    }
+
+    /**
+     *  Reacts to the card being tapped.
+     */
+    public void onTapCard(){
+
+    }
+
+    /**
+     * Reacts to a card being dismissed.
+     */
+    public void dismissCard(){
+
+    }
+}
