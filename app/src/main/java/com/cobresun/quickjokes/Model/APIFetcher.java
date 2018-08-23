11fc62059bd6ba2e91b3802f0d440947a79ef092
Sunny Nagam <sunnynagam1@gmail.com>
2018-08-22 22:12:30 -0600

    Init model implementation

--- app/src/main/java/com/cobresun/quickjokes/Model/APIFetcher.java
+++ app/src/main/java/com/cobresun/quickjokes/Model/APIFetcher.java
@@ -1,4 +1,6 @@
 package com.cobresun.quickjokes.Model;
 
 public interface APIFetcher {
+
+    String[][] getCardData(int numCards);
 }
---
+++ app/src/main/java/com/cobresun/quickjokes/Model/Impl/RedditAPIFetcher.java
@@ -0,0 +1,18 @@
+package com.cobresun.quickjokes.Model.Impl;
+
+import com.cobresun.quickjokes.Model.APIFetcher;
+
+public class RedditAPIFetcher implements APIFetcher{
+
+    private static String BASE_URL = "https://www.reddit.com/";
+    private static String SUBREDDIT = "r/jokes";
+
+    private static String getJSON(String targetUrl){
+
+    }
+
+    @Override
+    public String[][] getCardData(int numCards) {
+        return new String[0][];
+    }
+}
